2aacd2705a956e811ae0e4fb74ea1fa756a0617e
KOWI2003 <2003kowi@gmail.com>
2024-05-25 18:01:57 +0200

    Fixed Illegal Annotation

--- src/main/java/KOWI2003/LaserMod/utils/compat/cctweaked/LuaMap.java
+++ src/main/java/KOWI2003/LaserMod/utils/compat/cctweaked/LuaMap.java
@@ -2,11 +2,9 @@ package KOWI2003.LaserMod.utils.compat.cctweaked;
 
 import java.util.HashMap;
 
-import javax.annotation.Nonnull;
-
 import dan200.computercraft.api.lua.LuaTable;
 
 @SuppressWarnings("null")
-public class LuaMap<@Nonnull K, V> extends HashMap<K, V> implements LuaTable<K, V> {
+public class LuaMap<K, V> extends HashMap<K, V> implements LuaTable<K, V> {
     
 }
